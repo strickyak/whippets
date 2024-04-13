dcafc696f95fa2ce6d9abdbc9c331ddb8d46f438
Henry Strickland <strick@yak.net>
2024-04-13 13:59:54 -0700

    run-hasty.bash starts waiter and ends it.

--- run-hasty.bash
+++ run-hasty.bash
@@ -29,9 +29,19 @@ RAN=0
 
 rm -rfv /tmp/for-$T
 mkdir -p /tmp/for-$T
+
 cd $SHELF/gomar
 go build --tags=coco3,level2,vdg,cocoio,gime,$TRACE_TAGS  -o /tmp/for-$T/gomar.coco3.level2.vdg  gomar.go
 
+cd ../frobio/frob3/lemma/
+GOBIN=/home/strick/two/coco-shelf/bin GOPATH=/home/strick/two/coco-shelf /usr/bin/go build -o /tmp/for-$T/waiter -x server.go
+
+cd /tmp/for-$T/
+./waiter -cards=1 -ro=$SHELF/build-frobio/results/LEMMINGS -lan=127.0.0.1 -config_by_dhcp=0 >waiter.log 2>&1 &
+DAEMON=$!
+trap "EXIT=\$? ; kill $DAEMON ; exit \$EXIT" 0 1 2 3 15
+
+count=0
 for x in $TESTS
 do
 	rm -rfv /tmp/for-$T/$x
@@ -39,15 +49,6 @@ do
 	cd /tmp/for-$T/$x
 
 	cp -vf $SHELF/nitros9/level2/coco3/NOS9_6809_L2_80d.dsk L2.dsk
-
-	( cd /home/strick/go/src/github.com/strickyak/doing_os9/bootdisk_to_sbc09_file
-	  GOPATH=$SHELF go install bootdisk_to_sbc09_file.go
-	)
-	bootdisk_to_sbc09_file --level=2 < L2.dsk > L2.boot
-
-	#( cd /home/strick/go/src/github.com/strickyak/doing_os9/grok_os9_disk/ ; go build -x )
-	#/home/strick/go/src/github.com/strickyak/doing_os9/grok_os9_disk/grok_os9_disk < /tmp/L2.dsk > /tmp/for-$T-$x/manifest /tmp/for-$T-$x/disk
-
 	os9 copy -l -r $SHELF/whippets/hasty/$x/startup L2.dsk,startup
 
 	cp -v $SHELF/whippets/hasty/$x/inkey inkey
@@ -58,17 +59,9 @@ do
 	cp -v $SHELF/build-frobio/axiom41.rom axiom41.rom
 	cp -v $SHELF/build-frobio/axiom41.rom.list axiom41.rom.list
 
-	../gomar.coco3.level2.vdg  $TRACE_ARGS -disk L2.dsk  -rom_8000 coco3.rom -internal_rom_listing coco3.rom.list  -cart axiom41.rom  -external_rom_listing axiom41.rom.list --inkey_file=inkey -max 180000000 --show_vdg_screen=1 --bracket_terminal --expect='Level 2 V3;April 04, 2024  00:00;Shell+ v2.2a;{Term|02}/DD:' | tee _out
+	../gomar.coco3.level2.vdg  $TRACE_ARGS -disk L2.dsk  -rom_8000 coco3.rom -internal_rom_listing coco3.rom.list  -cart axiom41.rom  -external_rom_listing axiom41.rom.list --inkey_file=inkey -max 180'000'000 --show_vdg_screen=1 --bracket_terminal --expect='Level 2 V3;April 04, 2024  00:00;Shell+ v2.2a;{Term|02}/DD:' | tee _out
 
-	# ../gomar.coco3.level2.vdg  $TRACE_ARGS -disk L2.dsk  -rom_8000 coco3.rom -internal_rom_listing coco3.rom.list  -cart axiom41.rom  -external_rom_listing axiom41.rom.list --inkey_file=inkey -max 180000000 --show_vdg_screen=1 --bracket_terminal --expect="$(cat expect)" | tee _out
-
-	# ../gomar.coco3.level2.vdg  $TRACE_ARGS -disk L2.dsk  -rom_8000 coco3.rom -cart hdbsdc.rom  -max 50000000  --bracket_terminal --expect='Level 2 V3;Module Directory at;DeIniz;Free Blocks;megaread;Module Directory at;1 MDir;Shell+ v;Term'| tee _out
-
-	# ../gomar.coco3.level2.vdg  $TRACE_ARGS -boot /tmp/B2  -disk /tmp/L2.dsk  -max 50000000  --bracket_terminal --expect='Level 2 V3;Module Directory at;DeIniz;Free Blocks;megaread;Module Directory at;1 MDir;Shell+ v;Term'| tee _out
-
-	echo $T/$x OKAY.
+	count=$(( $count + 1 ))
+	echo "TEST (#$count) $T/$x OKAY." >&2
 done
-
-######################## OLD
-# go run -x --tags=coco3,level2,trace,d  gomar.go -t 1  -boot  ~/MIRROR/gomar-data/boot2coco3  -disk ~/coco-shelf/nitros9/level2/coco3/NOS9_6809_L2_80d.dsk | tee _out
-# time go run -x --tags=coco3,level2  gomar.go -boot  ~/MIRROR/gomar-data/boot2coco3  -disk /tmp/L2.dsk -max 50000000 --bracket_terminal --expect='Level 2 V3;Module Directory at;DeIniz;Free Blocks;megaread;Module Directory at;1 MDir;Shell+ v;Term'| tee _out
+echo RAN $count TESTS. >&2
